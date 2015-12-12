a5c3a55772621ba6c74138aae4cffda5fd6a4dcc
rospin8 <rospin8@gmail.com>
2015-12-12 23:09:11 +0300

    SaveImage(Only)
    aren’t working

---
+++ SaveImage(Only)/SaveImage/LoadViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LoadViewController.swift
+//  SaveImage
+//
+//  Created by Andrew Dzhur on 28/11/15.
+//  Copyright © 2015 AndrewDzhur. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class LoadViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    var imagePicker = UIImagePickerController()
+    
+    @IBAction func btnClicked(){
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum){
+            print("\nButton capture")
+            
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //loadImage()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+    func loadImage(){
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        
+        let request = NSFetchRequest(entityName: "StoreImages")
+        request.returnsObjectsAsFaults = false
+        //request.predicate = NSPredicate(format: <#T##String#>, <#T##args: CVarArgType...##CVarArgType#>)
+        
+        let result: NSArray = context.executeFetchRequest(request)
+        
+        if result.count > 0 {
+            imageView = (result[0] as! NSManagedObject).valueForKey("image")?.imageView
+        }
+        
+    }
+*/
+    
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in
+            
+        })
+        imageView.image = image
+        
+        print("Saving")
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        
+        let newImage = NSEntityDescription.insertNewObjectForEntityForName("StoreImages", inManagedObjectContext: context)
+        let im = UIImageJPEGRepresentation(image, 1)
+        newImage.setValue(UIImage(data: im!), forKey: "image")
+        
+        //context.save(nil)
+        
+        print(newImage)
+        print(image)
+    }
+}
