2f37eb82584e2b8a0206bda984e4f8fd566acacd
Igor Cova <Jin-Mac@MacBook-Pro-Igor.local>
2015-12-28 20:15:12 +0300

    Rename path

--- SaveImage/SaveImage/LoadViewController.swift
+++ SaveImage/SaveImage/LoadViewController.swift
@@ -32,12 +32,69 @@ class LoadViewController: UIViewController, UINavigationControllerDelegate, UIIm
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //saveFalseImage()
+        loadImage()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    func loadImage(){
+        
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName: "StoreImages")
+        
+        //3
+        do {
+            let results = try context.executeFetchRequest(fetchRequest) as! [StoreImages]
+            imageView.image  = UIImage(data: results[0].image!)
+    
+            
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        /*
+        
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        
+        let request = NSFetchRequest(entityName: "StoreImages")
+        request.returnsObjectsAsFaults = false
+        //request.predicate = NSPredicate(format: <#T##String#>, <#T##args: CVarArgType...##CVarArgType#>)
+        
+        do {
+            print("Пошлоооо")
+            let result: NSArray = try context.executeFetchRequest(request) as! [StoreImages]
+        
+            if result.count > 0 {
+                imageView.image = (result[1] as! NSManagedObject).valueForKey("image") as? UIImage
+            }
+        } catch {
+            print("Бляяяяя")
+        }
+        */
+    }
+/*
+    func saveFalseImage() {
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        let newImage = NSEntityDescription.insertNewObjectForEntityForName("StoreImages", inManagedObjectContext: context) as! StoreImages
+        
+        newImage.image = UIImageJPEGRepresentation(UIImage(named: "PICT0365.jpg")!, 1)
+        do {
+            try context.save()
+        } catch {
+            
+        }
+        
+    }
+*/
+
     
     
     func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
@@ -47,15 +104,19 @@ class LoadViewController: UIViewController, UINavigationControllerDelegate, UIIm
         imageView.image = image
         
         print("Saving")
-        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let context: NSManagedObjectContext = appDel.managedObjectContext
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        let newImage = NSEntityDescription.insertNewObjectForEntityForName("StoreImages", inManagedObjectContext: context) as! StoreImages
         
-        let newImage = NSEntityDescription.insertNewObjectForEntityForName("StoreImages", inManagedObjectContext: context)
-        newImage.setValue(image.description, forKey: "image")
+        newImage.image = UIImageJPEGRepresentation(image, 1)
         
-        //context.save(nil)
+        do {
+            try context.save()
+        } catch {
+            
+        }
         
-        print(image.description)
         print(newImage)
+        print(image)
     }
 }
