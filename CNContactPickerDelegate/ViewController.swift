03c2c0c78cc0f09bf65c64e70f8cc0a21e4d634a
rospin8 <rospin8@gmail.com>
2016-03-03 21:54:10 +0300

    CNContactPickerDelegate

---
+++ CNContactPickerDelegate/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  CNContactPickerDelegate
+//
+//  Created by Andrew Dzhur on 3/3/16.
+//  Copyright © 2016 Andrew Dzhur. All rights reserved.
+//
+
+import UIKit
+import Contacts
+import ContactsUI
+
+class ViewController: UIViewController, CNContactPickerDelegate {
+    
+    var contactStore = CNContactStore()
+    var updateContact = CNContact()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func click(sender: AnyObject) {
+        let contactPickerViewController = CNContactPickerViewController()
+        presentViewController(contactPickerViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Contact Access Permission Method
+    func askForContactAccess() {
+        let authorizationStatus = CNContactStore.authorizationStatusForEntityType(CNEntityType.Contacts)
+        
+        switch authorizationStatus {
+        case .Denied, .NotDetermined:
+            self.contactStore.requestAccessForEntityType(CNEntityType.Contacts, completionHandler: { (access, accessError) -> Void in
+                if !access {
+                    if authorizationStatus == CNAuthorizationStatus.Denied {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            let message = "\(accessError!.localizedDescription)\n\nPlease allow the app to access your contacts through the Settings."
+                            let alertController = UIAlertController(title: "Contacts", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                            
+                            let dismissAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (action) -> Void in
+                            }
+                            
+                            alertController.addAction(dismissAction)
+                            
+                            self.presentViewController(alertController, animated: true, completion: nil)
+                        })
+                    }
+                }
+            })
+            break
+        default:
+            break
+        }
+    }
+}
+
